9fa99dc93995138f71bb2f046f4d0eb17038280f
Maximilian Goisser <goisser94@gmail.com>
2017-05-19 19:41:27 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/render', function (req, res) {
+  console.log('Module ', req.body.module_path, '- render', 'pos', req.body.pos);
+  delete(require.cache[require.resolve(req.body.module_path)]);
+  var obj = require(req.body.module_path);
+
+  try {
+    var pos = req.body.pos || [];
+    var built = obj.build();
+    if (pos.length >= 1) {
+      for (var i = 0; i <= pos.length - 1; i++) {
+        built = built.props.children[pos[i]];
+      }
+    }
+
+    var result = built.render();
+    result.type = 'csg'; // needs to be added so it can be parsed as a CSG object
+    res.send(JSON.stringify(result));
+  } catch (e) {
+    console.error('ERROR', e);
+  }
+});
+
+console.log("Landau packager started on port 1938");
+app.listen(1938);
