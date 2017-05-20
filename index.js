d2674b9467b664daabce5374572564ed17560fb5
Maximilian Goisser <goisser94@gmail.com>
2017-05-20 10:23:31 +0200

    add /tree route; CLOSES #1

--- index.js
+++ index.js
@@ -4,6 +4,17 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.json());
 
+function buildWithPos(obj, pos) {
+  var built = obj.build();
+  if (pos.length >= 1) {
+    for (var i = 0; i <= pos.length - 1; i++) {
+      built = built.props.children[pos[i]];
+    }
+  }
+
+  return built;
+}
+
 app.post('/render', function (req, res) {
   console.log('Module ', req.body.module_path, '- render', 'pos', req.body.pos);
   delete(require.cache[require.resolve(req.body.module_path)]);
@@ -11,15 +22,26 @@ app.post('/render', function (req, res) {
 
   try {
     var pos = req.body.pos || [];
-    var built = obj.build();
-    if (pos.length >= 1) {
-      for (var i = 0; i <= pos.length - 1; i++) {
-        built = built.props.children[pos[i]];
-      }
-    }
+    var built = buildWithPos(obj, pos);
 
     var result = built.render();
     result.type = 'csg'; // needs to be added so it can be parsed as a CSG object
+    res.send(JSON.stringify(result));
+  } catch (e) {
+    console.error('ERROR', e);
+  }
+});
+
+app.post('/tree', function (req, res) {
+  console.log('Module ', req.body.module_path, '- tree', 'pos', req.body.pos);
+  delete(require.cache[require.resolve(req.body.module_path)]);
+  var obj = require(req.body.module_path);
+
+  try {
+    var pos = req.body.pos || [];
+    var built = buildWithPos(obj, pos);
+
+    var result = built.tree();
     res.send(JSON.stringify(result));
   } catch (e) {
     console.error('ERROR', e);
